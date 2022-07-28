6be5d81d6eecc3d7b0f94c3b4af612a25db4a837
Samuel Noye <samuel.noye@amalitech.org>
2022-07-28 10:02:20 +0000

    Update MutableAttributedString+Extention.swift

--- iOSExtensions/Extensions/MutableAttributedString+Extention.swift
+++ iOSExtensions/Extensions/MutableAttributedString+Extention.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 extension NSMutableAttributedString {
-    var fontSize: CGFloat { return 1 }
+    var fontSize: CGFloat { return 14 }
     
     func bold(_ value: String, boldFont: UIFont) -> NSMutableAttributedString {
         let attributes: [NSAttributedString.Key: Any] = [
